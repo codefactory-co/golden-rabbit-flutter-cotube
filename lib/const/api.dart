7084830518e777adf59fe5aadd55a3378f07fa6b
JC <withj1123@gmail.com>
2022-08-20 18:10:08 +0900

    done

---
+++ lib/const/api.dart
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyCeUyZpL-4QsgXD0HEz-EN04sYQwx6RFSs';
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
